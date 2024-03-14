7040254847b4ce3719bab1a68d73b0f44602ac38
SanthoshBoggala <bsanthoshbsr835@gmail.com>
2024-03-14 21:33:28 +0530

    modified some package-json for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const connectUrl = process.env.MONGODB_URL;
+const connectUrl = process.env.MONGO_ATLAS_URL;
 
 const userRoutes = require('./Routes/userRoutes');
 const productRoutes = require("./Routes/productRoutes");
