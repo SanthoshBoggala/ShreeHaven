a7c9c763b64c671627b353d9a7967bcb4bfb7598
SanthoshBoggala <bsanthoshbsr835@gmail.com>
2024-03-15 11:13:42 +0530

    removed some unwanted code

--- frontend/src/Components/Products/Products.js
+++ frontend/src/Components/Products/Products.js
@@ -30,46 +30,10 @@ const Products = ({ urlEndPoint, topRatedUrl = "", stylesForYouPage = false, top
     else{
         url = `https://shreehaven.onrender.com/api/products?search=${urlSearch}`   
     }
+    
     const { data: { products }, isLoading, error } = useFetchData({ url, query: filters, token })
 
-    // const { loading, data: {products}, error } = useGetData(url, filters, token);
-
-    // useEffect(() => {
-    //     async function getProducts() {
-    //         const products = await axios.get(`http://localhost:5000/api/products?type=${category}`, {
-    //             headers: {
-    //                 authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWMyZmNjNTczZDg2MjQyYTU0ZTIzNGUiLCJlbWFpbCI6InNAZ21haWwuY29tIiwidHlwZSI6ImFkbWluIiwiaWF0IjoxNzA4NTIwNTU0LCJleHAiOjE3MDg1NTY1NTR9.3t5fPvXA3A0jHXf67fsx7pQszT-jjjlVKocjc5sKfJA'
-    //             }
-    //         })
-
-    //         setProducts(products.data.products)
-    //     }
-    //     try {
-    //         getProducts()
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // }, [category, rating, filter])
-
-    // const Details = {
-    //     "productKey": "Men_striped_casual_light_green_white_shirt",
-    //     "name": "Men striped casual light green white shirt",
-    //     "brand": "U TURN",
-    //     "category": "Shirts",
-    //     "price": 369,
-    //     "newPrice": 313,
-    //     "discount": 15,
-    //     "starRating": 4,
-    //     "ratings": 30059,
-    //     "reviews": 3599,
-    //     "description": "Explore style with this men's striped casual light green and white shirt by U TURN. This shirt is perfect for a casual and trendy look.",
-    //     "image": "https://drive.google.com/uc?id=1TlgCuWATQr43Lxhn33i7DKgFeggLn9Al"
-    // }
-
-    // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 87, 43]
-    // const productDetails = arr.map(() => Details)
-
-
+   
     const searchChange = (e) => {
         setSearch(e.target.value)
     }
